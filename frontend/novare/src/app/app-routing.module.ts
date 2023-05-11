76ab1d3d622b01c73ad4a0acccd838a8a3f2c231
gilberto.junior <gilberto.junior@cbbasfaltos.com.br>
2023-05-11 18:04:51 -0300

    routing

--- frontend/novare/src/app/app-routing.module.ts
+++ frontend/novare/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { NewUserComponent } from './users/new-user/new-user.component';
 
 const routes: Routes = [
   {
