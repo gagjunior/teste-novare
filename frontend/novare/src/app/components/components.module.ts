c5397e1cd3ee3f9b88928344447b99f8a52d0bc5
gagjunior <gagjunior@hotmail.com>
2023-05-24 00:21:49 -0300

    feature: home component

--- frontend/novare/src/app/components/components.module.ts
+++ frontend/novare/src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
