bf2e0ee8fcee65d46c2d683a701f233045b24ce7
gilberto.junior <gilberto.junior@cbbasfaltos.com.br>
2023-06-01 17:45:09 -0300

    feature: Spring Config Server

--- backend/src/main/java/br/com/gagjunior/novare/controllers/UserController.java
+++ backend/src/main/java/br/com/gagjunior/novare/controllers/UserController.java
@@ -1,19 +1,27 @@
 package br.com.gagjunior.novare.controllers;
 
-import br.com.gagjunior.novare.configuration.NovareConfiguration;
-import br.com.gagjunior.novare.models.User;
-import br.com.gagjunior.novare.models.dao.UserDao;
-import br.com.gagjunior.novare.services.UserService;
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
+import br.com.gagjunior.novare.models.User;
+import br.com.gagjunior.novare.models.dao.UserDao;
+import br.com.gagjunior.novare.services.UserService;
 
 @CrossOrigin
 @RestController
@@ -22,8 +30,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    @Autowired
-    private NovareConfiguration configuration;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
@@ -33,11 +39,7 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> findAllUsers(
             @RequestParam(defaultValue = "0") Integer page,
-            @RequestParam(required = false) Integer size) {
-
-        if (size == null) {
-            size = configuration.getSize();
-        }
+            @RequestParam(defaultValue = "3") Integer size) {
 
         Pageable paging = PageRequest.of(page, size);
         return ResponseEntity.ok().body(userService.findAllUsers(paging));
@@ -46,7 +48,8 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody UserDao user) {
         User newUser = userService.saveNewUser(user);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(newUser.getId())
+                .toUri();
         return ResponseEntity.created(uri).body(newUser);
     }
 
@@ -61,6 +64,5 @@ public class UserController {
         User user = userService.updateUser(id, data);
         return ResponseEntity.ok(user);
     }
-
 
 }
