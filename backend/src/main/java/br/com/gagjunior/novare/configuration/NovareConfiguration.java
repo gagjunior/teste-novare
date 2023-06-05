7e1285bdbfd94426533a130b66c8a524bf7e2b07
gilberto.junior <gilberto.junior@cbbasfaltos.com.br>
2023-06-05 17:22:48 -0300

    configurations

--- backend/src/main/java/br/com/gagjunior/novare/configuration/NovareConfiguration.java
+++ backend/src/main/java/br/com/gagjunior/novare/configuration/NovareConfiguration.java
@@ -1,13 +1,15 @@
 package br.com.gagjunior.novare.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Configuration
-@ConfigurationProperties("config-service")
+@RefreshScope
+@ConfigurationProperties("novare-service")
 @Data
 @NoArgsConstructor
 public class NovareConfiguration {
